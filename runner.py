5f8c85098e5a45d7a50e33c0cc6006a820871ed3
NiuRunPeng <nrp1201@163.com>
2021-01-10 02:02:29 +0800

    测试

--- runner.py
+++ runner.py
@@ -3,5 +3,5 @@ import os
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(['-s', './pric.py', '--alluredir', 'allure_temp'])
-    os.system('allure serve allure_temp')
+    pytest.main(['-s', './pric.py', '--alluredir', 'temp'])
+    # os.system('allure serve temp')
